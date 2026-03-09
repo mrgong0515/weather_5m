8be383df3577ac29227d476cc2c2a0b2aae49683
공성우 <mrgong0515@gmail.com>
2026-03-09 16:43:42 +0900

    Comment out weather data fetching and CSV logic
    Commented out the entire weather data fetching and CSV writing logic.

--- weather.py
+++ weather.py
@@ -1,27 +1,27 @@
 # 날씨 정보를 요청해서 csv로 저장하는 코드
-import requests
-import csv
-from datetime import datetime
-import os
-MY_API_KEY = os.getenv("WEATHER_API_KEY")
+# import requests
+# import csv
+# from datetime import datetime
+# import os
+# MY_API_KEY = os.getenv("WEATHER_API_KEY")
 
-city_name = "Seoul"
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={MY_API_KEY}"
-url += "&units=metric"
-response = requests.get(url)
-result = response.json()
+# city_name = "Seoul"
+# url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={MY_API_KEY}"
+# url += "&units=metric"
+# response = requests.get(url)
+# result = response.json()
 
-main = result["weather"][0]["main"]       
-temp = result["main"]["temp"]            
-humidity = result["main"]["humidity"]   
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# main = result["weather"][0]["main"]       
+# temp = result["main"]["temp"]            
+# humidity = result["main"]["humidity"]   
+# current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-csv_exist = os.path.exists("weather.csv")
-header = ["current_time", "temp", "humidity", "main"]
-with open("weather.csv", "a", newline="") as f:
-    writer = csv.writer(f)
-    if not csv_exist:
-        writer.writerow(header)
+# csv_exist = os.path.exists("weather.csv")
+# header = ["current_time", "temp", "humidity", "main"]
+# with open("weather.csv", "a", newline="") as f:
+#     writer = csv.writer(f)
+#     if not csv_exist:
+#         writer.writerow(header)
 
-    writer.writerow([current_time, temp, humidity, main])
-print("날씨 저장 완료")
+#     writer.writerow([current_time, temp, humidity, main])
+# print("날씨 저장 완료")
